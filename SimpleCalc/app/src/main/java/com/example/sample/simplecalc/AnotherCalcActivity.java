78776b9a6224a538e330a6e96131d4e867fcea09
yoshifuji <yoshi2430jp@gmail.com>
2017-06-30 17:39:45 +0900

    MOD implement activities and their controllers

---
+++ SimpleCalc/app/src/main/java/com/example/sample/simplecalc/AnotherCalcActivity.java
@@ -0,0 +1,12 @@
+package com.example.sample.simplecalc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AnotherCalcActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
--- SimpleCalc/app/src/main/java/com/example/sample/simplecalc/MainActivity.java
+++ SimpleCalc/app/src/main/java/com/example/sample/simplecalc/MainActivity.java
@@ -2,12 +2,94 @@ package com.example.sample.simplecalc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements TextWatcher, View.OnClickListener{
+
+    private EditText numberInput1;
+    private EditText numberInput2;
+    private Spinner operatorSelector;
+    private TextView calcResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        numberInput1 = (EditText)findViewById(R.id.numberInput1);
+        numberInput1.addTextChangedListener(this);
+        numberInput2 = (EditText)findViewById(R.id.numberInput2);
+        numberInput2.addTextChangedListener(this);
+        operatorSelector = (Spinner)findViewById(R.id.operatorSelector);
+        calcResult = (TextView)findViewById(R.id.textResultNumber);
+
+        findViewById(R.id.calcButton).setOnClickListener(this);
+        findViewById(R.id.recalcButton).setOnClickListener(this);
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+
+    }
+
+    private void refreshResult(){
+        if (checkEditTextInput()){
+            String result = String.valueOf(calc());
+            calcResult.setText(result);
+        } else {
+            calcResult.setText(R.string.label_result_number);
+        }
+    }
+
+    private boolean checkEditTextInput(){
+        String input1 = numberInput1.getText().toString();
+        String input2 = numberInput2.getText().toString();
+        return !TextUtils.isEmpty(input1) && !TextUtils.isEmpty(input2);
+    }
+    private int calc(){
+        String input1 = numberInput1.getText().toString();
+        String input2 = numberInput2.getText().toString();
+
+        int num1 = Integer.parseInt(input1);
+        int num2 = Integer.parseInt(input2);
+        int oprt = operatorSelector.getSelectedItemPosition();
+        int result;
+
+        switch (oprt){
+            case 0: result = num1 + num2; break;
+            case 1: result = num1 - num2; break;
+            case 2: result = num1 * num2; break;
+            case 3: result = num1 / num2; break;
+            default:
+                throw new RuntimeException();
+        }
+
+        return result;
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+
+        switch (id){
+            case R.id.calcButton: refreshResult(); break;
+            case R.id.recalcButton: refreshResult(); break;
+        }
     }
 }
