296548d0923e0a653c220364f3e8b907a0a94502
yoshifuji <yoshi2430jp@gmail.com>
2017-07-07 17:59:16 +0900

    [mod][ongoing]

---
+++ MyMemoApp/app/src/main/java/com/example/sample/mymemoapp/MemoActivity.java
@@ -0,0 +1,10 @@
+package com.example.sample.mymemoapp;
+
+import android.app.Activity;
+
+/**
+ * Created by yoshitaka.fujisawa on 2017/07/07.
+ */
+
+public class MemoActivity extends Activity {
+}
