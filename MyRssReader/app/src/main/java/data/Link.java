b0169aa1d940887470d09662ed8c28e2bd05d767
yoshifuji <yoshi2430jp@gmail.com>
2017-07-09 22:38:59 +0900

    [add][ongoing]layout files

---
+++ MyRssReader/app/src/main/java/data/Link.java
@@ -0,0 +1,63 @@
+package data;
+
+/**
+ * Created by YoshitakaFujisawa on 2017/07/09.
+ */
+
+public class Link {
+
+    private long id;
+    private String title;
+    private String description;
+    private long pubData;
+    private String url;
+    private long siteid;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public long getPubData() {
+        return pubData;
+    }
+
+    public void setPubData(long pubData) {
+        this.pubData = pubData;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public long getSiteid() {
+        return siteid;
+    }
+
+    public void setSiteid(long siteid) {
+        this.siteid = siteid;
+    }
+}
---
+++ MyRssReader/app/src/main/java/data/Site.java
@@ -0,0 +1,54 @@
+package data;
+
+/**
+ * Created by YoshitakaFujisawa on 2017/07/09.
+ */
+
+public class Site {
+
+    private long id;
+    private String title;
+    private String description;
+    private String url;
+    private long linkCount;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public long getLinkCount() {
+        return linkCount;
+    }
+
+    public void setLinkCount(long linkCount) {
+        this.linkCount = linkCount;
+    }
+}
