874fe7caf892246b1ec83a5396a43acde5dac8c3
yoshifuji <yoshi2430jp@gmail.com>
2017-07-10 14:10:22 +0900

    [ongoing]dev adapter

--- MyRssReader/app/src/main/java/com/example/sample/myrssreader/adapter/LinkAdapter.java
+++ MyRssReader/app/src/main/java/com/example/sample/myrssreader/adapter/LinkAdapter.java
@@ -3,11 +3,16 @@ package com.example.sample.myrssreader.adapter;
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.TextView;
 
+import com.example.sample.myrssreader.R;
 import com.example.sample.myrssreader.data.Link;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -23,20 +28,98 @@ public class LinkAdapter extends RecyclerView.Adapter {
 
     private AdapterView.OnItemClickListener mListener;
 
+    public interface OnItemClickLisetner {
+        void OnItemClick(Link link);
+    }
 
+    public LinkAdapter(Context context) {
+        mContext = context;
+        mInflater = LayoutInflater.from(context);
+        mLinks = new ArrayList<>();
+    }
+
+    public void setOnItemClinkListener(AdapterView.OnItemClickListener listner) {
+        mListener = listner;
+    }
+
+    public void addItems(List<Link> links) {
+        mLinks.addAll(links);
+        notifyDataSetChanged();
+    }
+
+    public void remoteItem(long feedId) {
+        Iterator<Link> iterator = mLinks.iterator();
+        while (iterator.hasNext()) {
+            Link link = iterator.next();
+            if (link.getSiteid() == feedId) {
+                iterator.remove();
+            }
+        }
+        notifyDataSetChanged();
+    }
+
+    public void clearItems() {
+        mLinks.clear();
+        notifyDataSetChanged();
+    }
 
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        if (viewType == VIEW_TYPE_LINK) {
+            View view = mInflater.inflate(R.layout.item_link, parent, false);
+            return new LinkViewHolder(view, this);
+        }
+
         return null;
     }
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        if (holder instanceof LinkViewHolder) {
+            LinkViewHolder articleHolder = (LinkViewHolder) holder;
+            Link link = mLinks.get(position);
 
+            articleHolder.title.setText(link.getTitle());
+            articleHolder.description.setText(link.getDescription());
+            articleHolder.timeAgo.setText(mContext.getString(R.style.link_publish_date, link.getPubData()));
+        }
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mLinks.size();
     }
+
+    public int getItemViewType(int position) {
+        return VIEW_TYPE_LINK;
+    }
+
+    // ViewHolder for article ist
+    private static class LinkViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        private LinkAdapter adapter;
+        private TextView title;
+        private TextView description;
+        private TextView timeAgo;
+
+        public LinkViewHolder(View itemView, LinkAdapter adapter) {
+            super(itemView);
+
+            this.adapter = adapter;
+            title = (TextView) itemView.findViewById(R.id.Title);
+            description = (TextView) itemView.findViewById(R.id.Description);
+            timeAgo = (TextView) itemView.findViewById(R.id.TimeAgo);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            if(adapter.mListener != null){
+                int position = getLayoutPosition();
+                Link data = adapter.mLinks.get(position);
+                adapter.mListener.onItemClick(data);
+            }
+        }
+    }
+
 }
