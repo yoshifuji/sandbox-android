8ccbb6aa36acb2f4176feae2bbdb58aadf15e9fe
yoshifuji <yoshi2430jp@gmail.com>
2017-08-12 01:48:58 +0900

    [add]buttonClickListener

--- Slideshow/app/src/main/java/com/example/sample/slideshow/MainActivity.java
+++ Slideshow/app/src/main/java/com/example/sample/slideshow/MainActivity.java
@@ -7,13 +7,15 @@ package com.example.sample.slideshow;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.Window;
 import android.view.animation.AnimationUtils;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
@@ -24,21 +26,30 @@ import android.widget.ImageView;
 import android.widget.Toast;
 import android.widget.ViewSwitcher.ViewFactory;
 
-public class MainActivity extends Activity implements ViewFactory {
+import utils.Util;
+
+public class MainActivity extends Activity implements View.OnClickListener, ViewFactory {
 
     Gallery gallery;
     ImageSwitcher imageSwitcher;
     Context context;
     int imageItem;
+    int idxImage = 0; //選択中の画像Index
+
     Integer[] images = {
             R.drawable.image1,
             R.drawable.image2,
-            R.drawable.image3};
+            R.drawable.image3,
+            R.drawable.image4,
+            R.drawable.image5};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btn_back).setOnClickListener(this);
+        findViewById(R.id.btn_next).setOnClickListener(this);
 
         context = MainActivity.this;
 
@@ -54,10 +65,47 @@ public class MainActivity extends Activity implements ViewFactory {
 
         imageSwitcher = (ImageSwitcher) findViewById(R.id.imageSwitcher);
         imageSwitcher.setFactory(MainActivity.this);
+
+        //Setting 'In Animation'
         imageSwitcher.setInAnimation(AnimationUtils.loadAnimation(MainActivity.this, android.R.anim.fade_in));
+        //Setting 'Out Animation'
         imageSwitcher.setOutAnimation(AnimationUtils.loadAnimation(MainActivity.this, android.R.anim.fade_out));
+
         imageSwitcher.setAlpha(Float.parseFloat("50.0"));
 
+        //img = (ImageView)findViewById(R.id.imageSwitcher);
+        //img.setOnTouchListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+
+        switch (id){
+            case R.id.btn_next:
+                idxImage = (idxImage + 1 < images.length) ? idxImage + 1 : idxImage;
+                imageSwitcher.setImageResource(images[idxImage]);
+
+                /*
+                todo set updated filename
+                 */
+//                Util ut = new Util();
+//                if(ut.checkEditTextInput(numberInput1, numberInput2)){
+//                    int result = ut.calc(numberInput1, numberInput2, operatorSelector);
+//                    tv_filename.setText(String.valueOf(result));
+//                }
+                break;
+            case R.id.btn_back:
+                idxImage = (idxImage > 0) ? idxImage - 1 : idxImage;
+                imageSwitcher.setImageResource(images[idxImage]);
+
+                /*
+                todo set updated filename
+                 */
+
+                break;
+        }
     }
 
     @Override
---
+++ Slideshow/app/src/main/java/utils/Util.java
@@ -0,0 +1,30 @@
+package utils;
+
+/**
+ * Created by YoshitakaFujisawa on 2017/08/12.
+ */
+
+public class Util {
+
+    /*
+    todo Intent経由でPreferenceデータを取得する
+     */
+    public String hoge(){
+//            case R.id.precalcButton:
+//                Intent intent1 = new Intent(this, AnotherCalcActivity.class);
+//                startActivityForResult(intent1, REQUEST_CODE_ANOTHER_CALC_1);
+//                break;
+        return "";
+    }
+
+    /*
+    todo 表示されている画像ファイル名を更新する
+     */
+    public void fuga(){
+
+    }
+
+}
+
+
+
