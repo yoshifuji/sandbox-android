d0cf1242e67609b6a8519503ad50a9d4ccd5b66c
yoshifuji <yoshi2430jp@gmail.com>
2017-08-19 03:19:36 +0900

    buggy but to grasp development summary

---
+++ VoiceRecognition/app/src/main/java/com/example/sample/voicerecognition/MainActivity.java
@@ -0,0 +1,86 @@
+package com.example.sample.voicerecognition;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.speech.RecognizerIntent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.sample.voicerecognition.R.id;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    // = 0 の部分は、適当な値に変更してください（とりあえず試すには問題ないですが）
+    private static final int REQUEST_CODE = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    // インテント作成
+                    Intent intent = new Intent(
+                            RecognizerIntent.ACTION_RECOGNIZE_SPEECH); // ACTION_WEB_SEARCH
+                    intent.putExtra(
+                            RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                            RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                    intent.putExtra(
+                            RecognizerIntent.EXTRA_PROMPT,
+                            "VoiceRecognitionTest"); // お好きな文字に変更できます
+
+                    // インテント発行
+                    startActivityForResult(intent, REQUEST_CODE);
+                } catch (ActivityNotFoundException e) {
+                    // このインテントに応答できるアクティビティがインストールされていない場合
+//                    Toast.makeText(VoiceRecognitionTestActivity.this,
+//                            "ActivityNotFoundException", Toast.LENGTH_LONG).show();
+                    showMessage("VoiceRecognitionActivity NotFoundException");
+                }
+            }
+        });
+
+    }
+
+    // アクティビティ終了時に呼び出される
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // 自分が投げたインテントであれば応答する
+        if (requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
+            String resultsString = "";
+
+            // 結果文字列リスト
+            ArrayList<String> results = data.getStringArrayListExtra(
+                    RecognizerIntent.EXTRA_RESULTS);
+
+            for (int i = 0; i< results.size(); i++) {
+                // ここでは、文字列が複数あった場合に結合しています
+                resultsString += results.get(i);
+            }
+
+            // トーストを使って結果を表示
+            Toast.makeText(this, resultsString, Toast.LENGTH_LONG).show();
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    /*
+    Toastメッセージ表示
+    */
+    protected void showMessage(String msg) {
+        Toast.makeText(
+                this,
+                msg, Toast.LENGTH_SHORT).show();
+    }
+}
